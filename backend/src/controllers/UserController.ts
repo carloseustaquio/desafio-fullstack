6e969afbc8b5cc07e166f7ff6fa83c5f435aff8f
Carlos Eustáquio <caeu227@gmail.com>
2020-09-17 13:04:02 -0300

    add custom errors to api

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,18 +1,20 @@
-import { Controller, Post } from "@overnightjs/core";
+import { ClassErrorMiddleware, Controller, Post } from "@overnightjs/core";
 import { UserRepository } from "@src/repositories/implementations/UserRepository";
 import { CreateUser } from "@src/useCases/CreateUser";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { errorMiddleware } from "@src/middlewares/errorMiddleware"
 
 @Controller("user")
+@ClassErrorMiddleware(errorMiddleware)
 export class UserController {
   @Post("")
-  public async postUser(req: Request, res: Response): Promise<void> {
+  public async postUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const createUserUseCase = new CreateUser(new UserRepository())
       const userCreated = await createUserUseCase.execute(req.body)
       res.status(201).send(userCreated);
     } catch (error) {
-      res.status(500).send({ error: "Error while trying to create user." });
+      next(error)
     }
   }
 }
--- backend/src/helpers/Validation.ts
+++ backend/src/helpers/Validation.ts
@@ -1,13 +1,16 @@
+import { ValidationError } from "@src/util/errors/ValidationError"
+
 export class Validation {
   static validateFields(data: any, requiredFields: Array<any>) {
     const missingFields = requiredFields.filter(requiredField => {
+
       // check for falsy values except 'false' (boolen)
       if (data[requiredField] || data[requiredField] === false) { return }
       else { return requiredField }
     })
 
     if (missingFields.length) {
-      throw new Error(`Missing fields: ${missingFields.join(", ")}.`)
+      throw new ValidationError(`Missing fields: ${missingFields.join(", ")}.`)
     }
   }
 }
