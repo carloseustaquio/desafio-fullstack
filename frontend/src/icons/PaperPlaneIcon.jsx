0e3bb507340850b411ef5bc842780edddbfe7c34
Carlos Eustáquio <caeu227@gmail.com>
2020-09-18 20:00:52 -0300

    add paper plane icon

---
+++ frontend/src/icons/PaperPlaneIcon.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PaperPlaneIcon = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0)">
+        <path d="M14.875 0.100029L0.390666 8.45628C-0.174959 8.78128 -0.103084 9.56878 0.459416 9.80628L3.78129 11.2L12.7594 3.28753C12.9313 3.1344 13.175 3.36878 13.0282 3.5469L5.50004 12.7188V15.2344C5.50004 15.9719 6.39067 16.2625 6.82817 15.7282L8.81254 13.3125L12.7063 14.9438C13.15 15.1313 13.6563 14.8532 13.7375 14.375L15.9875 0.875029C16.0938 0.243779 15.4157 -0.212471 14.875 0.100029Z" fill="white" />
+      </g>
+      <defs>
+        <clipPath id="clip0">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+
+  )
+}
+
+export default PaperPlaneIcon
