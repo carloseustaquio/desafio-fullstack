3a4c51959127b45f38472c2eb688255bc9b0ffbe
Carlos Eustáquio <caeu227@gmail.com>
2020-09-20 18:43:16 -0300

    add mobile option to api axios

--- frontend/src/functions/api.js
+++ frontend/src/functions/api.js
@@ -2,4 +2,5 @@ import axios from "axios"
 
 export default axios.create({
   baseURL: `http://localhost:8080`
+  // baseURL: `http://192.168.0.107:8080` // active this to test directly on mobile
 })
