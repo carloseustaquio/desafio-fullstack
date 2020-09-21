1a4ffe4f490a7b79b15b5fe1841a58ed6f4c1786
Carlos Eustáquio <caeu227@gmail.com>
2020-09-21 07:43:50 -0300

    update comment

--- frontend/src/functions/api.js
+++ frontend/src/functions/api.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export default axios.create({
   baseURL: `http://localhost:8080`
-  // baseURL: `http://192.168.0.107:8080` // active this to test directly on mobile
+  // baseURL: `http://192.168.0.107:8080` // active local ip to test directly on mobile device
 })
